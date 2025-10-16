eb22548f8eef217417f7e03465af88d20ce8cd82
s-alice.pizzini@istitutopilati.it <s-alice.pizzini@istitutopilati.it>
2025-10-16 14:54:44 +0200

    v4

--- cpp/prova/main.cpp
+++ cpp/prova/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
-//using namesace std;
+using namespace std;
+
+class Counter{
+
+    private:
+        int value;
+
+    public:    
+        Counter(){ 
+            cout << "istanza dell'oggetto creata \n";
+            value = 0;
+        }
+        ~Counter(){ cout << "oggetto istrutto \n"; }
+        int getValue(){ 
+            return value;
+        }
+        void increase(int n){ 
+            value += n;
+        } 
+};
+
+
 
 int main(){
 
-   std::cout << "Ciao mondo!";
+   Counter c1,c2;
+
+   cout << "Contatore 1: " << c1.getValue() <<"\n";
+   cout << "Contatore 2: " << c2.getValue() <<"\n";
+
+   c1.increase(7);
+
+   cout << "Contatore 1: " << c1.getValue() <<"\n";
+   cout << "Contatore 2: " << c2.getValue() <<"\n";
 
     return 0;
 }
